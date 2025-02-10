a7e495842c4b332fe90ae2c84b1b2d1f44f5dea6
OhMyApps <74984020+GiGiDKR@users.noreply.github.com>
2025-02-10 02:26:04 +0100

    0.1.7

--- OhMyWindows/Services/AppNotificationService.cs
+++ OhMyWindows/Services/AppNotificationService.cs
@@ -28,28 +28,43 @@ public class AppNotificationService : IAppNotificationService
 
         AppNotificationManager.Default.Register();
 
-        // Afficher une notification toast au démarrage
-        Show("Bienvenue sur OhMyWindows");
+        // Afficher une notification toast au démarrage avec un format amélioré
+        ShowWelcomeNotification();
+    }
+
+    private void ShowWelcomeNotification()
+    {
+        var toastXml = $@"<toast duration='long'>
+            <visual>
+                <binding template='ToastGeneric'>
+                    <text>Bienvenue sur OhMyWindows</text>
+                    <text>Votre assistant de gestion des applications Windows</text>
+                </binding>
+            </visual>
+            <actions>
+                <action content='Ouvrir' arguments='action=open'/>
+                <action content='Fermer' arguments='action=dismiss'/>
+            </actions>
+        </toast>";
+
+        Show(toastXml);
     }
 
     public void OnNotificationInvoked(AppNotificationManager sender, AppNotificationActivatedEventArgs args)
     {
-        // TODO: Handle notification invocations when your app is already running.
-
-        //// // Navigate to a specific page based on the notification arguments.
-        //// if (ParseArguments(args.Argument)["action"] == "Settings")
-        //// {
-        ////    App.MainWindow.DispatcherQueue.TryEnqueue(() =>
-        ////    {
-        ////        _navigationService.NavigateTo(typeof(SettingsViewModel).FullName!);
-        ////    });
-        //// }
+        var arguments = ParseArguments(args.Argument);
+        var action = arguments["action"];
 
         App.MainWindow.DispatcherQueue.TryEnqueue(() =>
         {
-            App.MainWindow.ShowMessageDialogAsync("TODO: Handle notification invocations when your app is already running.", "Notification Invoked");
-
-            App.MainWindow.BringToFront();
+            if (action == "open")
+            {
+                App.MainWindow.BringToFront();
+            }
+            else if (action == "dismiss")
+            {
+                App.MainWindow.Close();
+            }
         });
     }
 
